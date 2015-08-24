92ab2691986bf46b35a07aad815480d660f4187d
amalankrish <amalan.krish@gmail.com>
2015-08-24 18:00:15 -0400

    JSoup Webcrawler
    Application that fetches results (links and corresponding webpage
    titles) from Yahoo, Wikipedia, and Bing using a Java HTML parser given
    a search parameter

---
+++ src/Crawler.java
@@ -0,0 +1,15 @@
+
+public class Crawler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+            WebCrawler wc=new WebCrawler();
+            wc.setVisible(true);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+	}
+
+}
